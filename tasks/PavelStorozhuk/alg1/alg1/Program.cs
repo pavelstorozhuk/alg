cf3c602b30b381bf78383579ae5a77bdb73d6eb4
pavelstorozhuk <pavelstorozhuk94@gmail.com>
2016-10-30 23:02:01 +0200

    Add files via upload

---
+++ tasks/PavelStorozhuk/alg1/alg1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using System.Threading;
+
+namespace alg1
+{
+    class Program
+    {
+        static UInt32 getNumberOfDigits(UInt64 n)
+        {
+            
+            UInt32 result = 1;
+            for (; ; )
+            {
+                Console.WriteLine("step");
+                if (n < 10)
+                {
+                    return result;
+                }
+                if (n < 100)
+                {
+                    return result + 1;
+                }
+                if (n < 1000)
+                {
+                    return result + 2;
+                }
+                if (n < 10000)
+                {
+                    return result + 3;
+                }
+                n /= 10000;
+                result += 4;
+              
+            }
+        }
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            Console.WriteLine("3");
+            stopwatch.Start();
+            getNumberOfDigits(123);
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+            Console.WriteLine("------------");
+            Console.WriteLine("4");
+            getNumberOfDigits(1234);
+            Console.WriteLine("------------");
+            Console.WriteLine("5");
+            getNumberOfDigits(12345);
+            Console.WriteLine("------------");
+            Console.WriteLine("6");
+            getNumberOfDigits(123456);
+            Console.WriteLine("------------");
+            Console.WriteLine("7");
+            getNumberOfDigits(1234567);
+            Console.WriteLine("------------");
+            Console.WriteLine("8");
+            getNumberOfDigits(12345678);
+            Console.WriteLine("------------");
+            Console.WriteLine("9");
+            getNumberOfDigits(123456789);
+            Console.WriteLine("------------");
+            Console.WriteLine("10");
+            getNumberOfDigits(1234567890);
+            Console.WriteLine("------------");
+            Console.ReadKey();
+        }
+    }
+}
