f7f50c9d454e89f850134a0bbbae8fa49f788b4b
pavelstorozhuk <pavelstorozhuk94@gmail.com>
2016-11-06 22:39:34 +0200

    Add files via upload

---
+++ tasks/PavelStorozhuk/Algor1Final/Algor1Final/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algor1Final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary dictionary = new MyDictionary(354969,@".\dict_en.txt");
+            dictionary.Load();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.WriteLine(TextSegmintation.WordSegmintaion("abactorabashedsublativesubline",dictionary));
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+            Console.WriteLine(stopwatch.Elapsed);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tasks/PavelStorozhuk/Algor1Final/Algor1Final/TextSegmintation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Algor1Final
+{
+    public static class TextSegmintation
+    {
+        public static string WordSegmintaion(string text, MyDictionary dictionary)
+        {
+            var result = String.Empty;
+            var buf = string.Empty;
+            for (int i = 0; i < text.Length; i++)
+            {
+                buf += text[i];
+
+                {
+                    if (dictionary.Contains(buf))
+                    {
+                     
+                        {
+                            result += buf + " ";
+                            buf = string.Empty;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
