49244f614fb832a5aa8e2da52569105cfa68dc2e
pavelstorozhuk <pavelstorozhuk94@gmail.com>
2016-11-07 18:36:30 +0200

    Task1 &&Task2

---
+++ tasks/PavelStorozhuk/AlgTask2/AlgTask2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgTask2
+{
+    class Program
+    {
+        public static string LCS(string s1, string s2)
+        {
+            int n = s1.Length;
+            int m = s2.Length;
+
+            int[,] lcs = new int[n + 1, m + 1];
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    if (s1[i - 1].Equals(s2[j - 1]))
+                        lcs[i, j] = lcs[i - 1, j - 1] + 1;
+                    else
+                        lcs[i, j] = Math.Max(lcs[i - 1, j], lcs[i, j - 1]);
+                }
+            }
+
+            int index = lcs[n, m];
+            char[] result = new char[index + 1];
+
+            int k = n;
+            int l = m;
+
+            while (k > 0 && l > 0)
+            {
+                if (s1[k - 1].Equals(s2[l - 1]))
+                {
+                    result[index - 1] = s1[k - 1];
+                    k--;
+                    l--;
+                    index--;
+                }
+                else if (lcs[k - 1, l].CompareTo(lcs[k, l - 1]) == 1)
+                    k--;
+                else
+                    l--;
+            }
+
+            return new string(result);
+
+          
+        }
+        static void Main(string[] args)
+        {
+            string s1 = "ABECDGH";
+            string s2 = "AEDFDHR";
+            Console.WriteLine(LCS(s1, s2));
+            Console.ReadKey();
+        }
+       
+    }
+}
---
+++ tasks/PavelStorozhuk/Algor1Final/Algor1Final/MyDictionary.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algor1Final
+{
+    public class MyDictionary
+    {
+        private class DictionaryEntry
+        {
+            //354969
+            public int Key { get; set; }
+            public string Value { get; private set; }
+          
+            public DictionaryEntry(string val)
+            {
+               
+                Value = val;
+            }
+            public DictionaryEntry(int key,string val)
+            {
+
+                Value = val;
+                Key = key;
+            }
+        }
+        private FileStream _stream;
+        private StreamReader _reader;
+        private LinkedList<DictionaryEntry>[] _array;
+        private int _capacity;
+        private int _size;
+        private const double LoadFactor = 0.75;
+
+        public MyDictionary(int size, string path)
+        {
+            _capacity = size;
+            _array = new LinkedList<DictionaryEntry>[_capacity];
+            _stream = File.OpenRead(path);
+            _reader = new StreamReader(_stream);
+        }
+
+        public void Load()
+        {
+           
+            while (!_reader.EndOfStream)
+            {
+                string word = _reader.ReadLine();
+               
+                if (word != "")
+                {
+                    Add(word);
+
+                }
+
+            }
+        }
+        public MyDictionary()
+        {
+            _capacity = 5;
+            _array = new LinkedList<DictionaryEntry>[_capacity];
+        }
+        private int Hash(string key)
+        {
+
+            return Math.Abs(key.GetHashCode()) % _capacity;
+        }
+
+        private double GetLoadFactor()
+        {
+            return _size / _capacity;
+        }
+
+        private void Resize()
+        {
+            _capacity = _capacity * 2;
+            var oldArray = _array;
+            _size = 0;
+            _array = new LinkedList<DictionaryEntry>[_capacity];
+
+            foreach (var item in oldArray)
+            {
+                if (item != null)
+                {
+                    foreach (var pair in item)
+                    {
+                        if (pair != null)
+                            Add(pair.Value);
+                    }
+                }
+            }
+        }
+
+       
+        public void Add( string val)
+        {
+            if (GetLoadFactor() >= LoadFactor)
+            {
+                Resize();
+            }
+
+            var index = Hash(val);
+
+            if (_array[index] == null)
+            {
+                _array[index] = new LinkedList<DictionaryEntry>();
+            }
+
+            var hashTableItem = new DictionaryEntry(index, val);
+
+            var listNode = new LinkedListNode<DictionaryEntry>(hashTableItem);
+
+            _array[index].AddFirst(listNode);
+
+            _size++;
+        }
+
+       
+        public bool Contains(string value)
+        {
+            int index = Hash(value);
+            if (_array[index] != null)
+            {
+               var buf=   _array[index].Where(m => m.Value == value);
+                if (buf.FirstOrDefault() != null)
+                    return true;
+            }
+            return false;
+        }
+       
+    }
+}
