4c980611b9056f05a24c66c3f7fb1d103f7d5902
pavelstorozhuk <pavelstorozhuk94@gmail.com>
2016-11-30 23:32:40 +0200

    Task4

---
+++ tasks/PavelStorozhuk/AlgTask4/AlgTask4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 100);
+            }
+            HeapSort heapSort = new HeapSort(10);
+            for (int i = 0; i < array.Length; i++)
+            {
+                heapSort.Push(array[i]);
+            }
+            heapSort.Print();
+
+           for (int i = 0; i < array.Length; i++)
+           {
+                Console.Write(heapSort.Pop()+" ");
+           }
+            Console.ReadKey();
+        }
+    }
+}
